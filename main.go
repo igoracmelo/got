9706e1e634c129e04f7b8d9e988c44a7ae8e90a1
Igor Melo <igoracmelo@protonmail.com>
2023-02-17 13:37:00 -0300

    --wip-- [skip ci]

--- main.go
+++ main.go
@@ -13,14 +13,23 @@ import (
 func main() {
 	g := &core.Got{
 		Repositories: []core.GitRepository{
-			{Dir: "/home/igor/Git/essia/essia-frontend-core"},
-			{Dir: "/home/igor/Git/essia/essia-frontend-backoffice"},
-			{Dir: "/home/igor/Git/essia/essia-editor"},
-			{Dir: "/home/igor/Git/essia/essia-mobile"},
-			{Dir: "/home/igor/Git/essia/essia-desktop"},
-			{Dir: "/home/igor/Git/essia/essia-backend-catalog"},
 			{Dir: "/home/igor/Git/essia/essia-backend-auth"},
+			{Dir: "/home/igor/Git/essia/essia-backend-catalog"},
+			{Dir: "/home/igor/Git/essia/essia-backend-classwork"},
+			{Dir: "/home/igor/Git/essia/essia-backend-ecommerce"},
+			{Dir: "/home/igor/Git/essia/essia-backend-feature-flag"},
+			{Dir: "/home/igor/Git/essia/essia-backend-media-optimizer"},
+			{Dir: "/home/igor/Git/essia/essia-backend-pdf"},
 			{Dir: "/home/igor/Git/essia/essia-backend-repository"},
+			{Dir: "/home/igor/Git/essia/essia-backend-school-system"},
+			{Dir: "/home/igor/Git/essia/essia-backend-trail"},
+			{Dir: "/home/igor/Git/essia/essia-desktop"},
+			{Dir: "/home/igor/Git/essia/essia-editor"},
+			{Dir: "/home/igor/Git/essia/essia-frontend-backoffice"},
+			{Dir: "/home/igor/Git/essia/essia-frontend-core"},
+			{Dir: "/home/igor/Git/essia/essia-frontend-ecommerce"},
+			{Dir: "/home/igor/Git/essia/essia-frontend-pdf-extractor"},
+			{Dir: "/home/igor/Git/essia/essia-mobile"},
 		},
 	}
 
@@ -29,6 +38,8 @@ func main() {
 
 	for res := range results {
 		// os.Stdout.Write(res.Stdout)
+		fmt.Println(strings.Repeat("-", 100))
+		// fmt.Println(strings.Repeat("-", 30))
 		if res.Error != nil {
 			fmt.Println("\033[1;31m[x] \033[1;37m" + path.Base(res.Repository.Dir) + "\033[0m")
 		} else {
@@ -36,18 +47,18 @@ func main() {
 		}
 
 		stdout := string(res.Stdout)
-		stdoutLines := strings.Split(stdout, "\n")
+		// stdoutLines := strings.Split(stdout, "\n")
 
-		if len(stdoutLines) > 10 {
-			stdout = strings.Join(stdoutLines[:10], "\n") + "\n(...)\n"
-		}
+		// if len(stdoutLines) > 10 {
+		// 	stdout = strings.Join(stdoutLines[:10], "\n") + "\n(...)\n"
+		// }
 
 		stderr := string(res.Stderr)
-		stderrLines := strings.Split(stderr, "\n")
+		// stderrLines := strings.Split(stderr, "\n")
 
-		if len(stderrLines) > 10 {
-			stderr = strings.Join(stderrLines[:10], "\n") + "\n(...)\n"
-		}
+		// if len(stderrLines) > 10 {
+		// 	stderr = strings.Join(stderrLines[:10], "\n") + "\n(...)\n"
+		// }
 
 		// stdout := strings.Join(strings.Split(string(res.Stdout), "\n")[:10], "\n")
 		// stderr := strings.Join(strings.Split(string(res.Stderr), "\n")[:10], "\n")
@@ -58,9 +69,10 @@ func main() {
 		// os.Stderr.Write(res.Stderr)
 
 		if len(res.Stdout) == 0 && len(res.Stderr) == 0 {
-			fmt.Println("(vazio)")
+			fmt.Println("(empty)")
 		}
-		fmt.Println()
+		fmt.Println(strings.Repeat("-", 100))
+		// fmt.Println()
 		fmt.Println()
 	}
 }
